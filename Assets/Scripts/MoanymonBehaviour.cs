a9ea9f2c065c6da6b1e2ed917cdfa10b5d4c54ef
Safder Iqbal <safder.iqbal@intechnica.co.uk>
2017-10-28 22:18:40 +0100

    Added Letter prefabs, fixed click events, created a test scene and sorted colliders

--- Assets/Scripts/MoanymonBehaviour.cs
+++ Assets/Scripts/MoanymonBehaviour.cs
@@ -1,27 +1,30 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Linq;
+using UnityEngine;
 
-namespace Assets.Scripts
+public class MoanymonBehaviour: MonoBehaviour
 {
-    public class MoanymonBehaviour: MonoBehaviour
+    public GameObject Camera;
+
+    public void Start()
     {
-        public GameObject Camera;
+        Camera = GameObject.FindGameObjectWithTag("MainCamera");
+    }
 
-        public void Start()
+    //public bool DetectClick(string[] objects)
+    public bool DetectClick()
+    {
+        // if left button pressed...
+        if (!Input.GetMouseButtonDown(0))
         {
-            Camera = GameObject.FindGameObjectWithTag("MainCamera");
+            return false;
         }
 
-        public bool DetectClick()
-        {
-            // if left button pressed...
-            if (!Input.GetMouseButtonDown(0))
-            {
-                return false;
-            }
+        Ray ray = Camera.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit);
 
-            Ray ray = Camera.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            return Physics.Raycast(ray, out hit);
-        }
+        return gameObject == hit.transform.gameObject;
     }
 }
+
