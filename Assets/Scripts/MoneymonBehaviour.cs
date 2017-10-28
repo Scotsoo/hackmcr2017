46d4e1fc5b64aaa9c5b11bbda4d5d12d8d150579
Safder Iqbal <safder.iqbal@intechnica.co.uk>
2017-10-28 17:42:42 +0100

    Extended MonoBehaviour to add click handler and made player click handler

---
+++ Assets/Scripts/MoneymonBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class MoneymonBehaviour: MonoBehaviour
+    {
+        public GameObject Camera;
+
+        public void Start()
+        {
+            Camera = GameObject.FindGameObjectWithTag("MainCamera");
+        }
+
+        public bool DetectClick()
+        {
+            // if left button pressed...
+            if (!Input.GetMouseButtonDown(0))
+            {
+                return false;
+            }
+
+            Ray ray = Camera.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            return Physics.Raycast(ray, out hit);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerActions/PlayerClick.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.PlayerActions
+{
+    public class PlayerClick : MoneymonBehaviour
+    {
+        //// Use this for initialization
+        //void Start()
+        //{
+        //    base.Start();
+        //}
+
+        // Update is called once per frame
+        void Update()
+        {
+            OnClick();
+        }
+
+        public void OnClick()
+        {
+            if (DetectClick())
+            {
+                Debug.Log("Raycast hit!");
+            }
+        }
+    }
+}
