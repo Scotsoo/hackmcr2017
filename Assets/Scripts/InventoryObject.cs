a374b741b4d3230130ad455ade5f74ec82fea80a
Safder Iqbal <safder.iqbal@intechnica.co.uk>
2017-10-29 06:58:47 +0000

    Enhanced Inventory object to hold count of duplicate characters

---
+++ Assets/Scripts/InventoryObject.cs
@@ -0,0 +1,14 @@
+﻿namespace Assets.Scripts
+{
+    public class InventoryObject
+    {
+        public string Character { get; set; }
+
+        public int CaptureCount { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Character: {0}, Count: {1}", Character, CaptureCount);
+        }
+    }
+}
