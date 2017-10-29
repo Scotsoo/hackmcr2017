f3645bf2c6c8a0b16b2c2d2df58954c555c68434
Safder Iqbal <safder.iqbal@intechnica.co.uk>
2017-10-29 07:00:41 +0000

    Added field for future expansion

--- Assets/Scripts/InventoryObject.cs
+++ Assets/Scripts/InventoryObject.cs
@@ -6,6 +6,9 @@
 
         public int CaptureCount { get; set; }
 
+        // TODO Implement this in the form submission
+        public int FontSize { get; set; }
+
         public override string ToString()
         {
             return string.Format("Character: {0}, Count: {1}", Character, CaptureCount);
