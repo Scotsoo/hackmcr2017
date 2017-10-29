1a5cf2e5bfb31b8420eb48be6c7e81d3f627ce53
Safder Iqbal <safder.iqbal@intechnica.co.uk>
2017-10-29 05:58:05 +0000

    Added basic on screen movement keys

---
+++ Assets/Scripts/PlayerPinOnScreenMovement.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class PlayerPinOnScreenMovement : MoanymonBehaviour
+{
+    public float Speed = 0.1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        base.Start();
+
+    }
+
+    //// Update is called once per frame
+    //void Update () {
+
+    //}
+
+    public void Up_OnClick()
+    {
+        Vector3 velocity = Vector3.zero;
+
+        velocity += new Vector3(Speed, 0, 0);
+
+        OnClickUpdate(velocity);
+    }
+
+    public void Down_OnClick()
+    {
+        Vector3 velocity = Vector3.zero;
+
+        velocity -= new Vector3(Speed, 0, 0);
+
+        OnClickUpdate(velocity);
+    }
+
+    public void Left_OnClick()
+    {
+        Vector3 velocity = Vector3.zero;
+
+        velocity += new Vector3(0, 0, Speed);
+
+        OnClickUpdate(velocity);
+    }
+
+    public void Right_OnClick()
+    {
+        Vector3 velocity = Vector3.zero;
+
+        velocity -= new Vector3(0, 0, Speed);
+
+        OnClickUpdate(velocity);
+    }
+
+    private void OnClickUpdate(Vector3 velocity)
+    {
+        transform.position += velocity;
+        Camera.transform.position += velocity;
+    }
+}
